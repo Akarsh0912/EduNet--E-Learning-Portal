056a52746c65c9fb4069d0cee14f4d3a6ff587c4
Akarsh <akarshgupta816@gmail.com>
2024-04-10 13:28:17 +0530

    settings Update

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-m#vo!maof_7urie-^xn^##gcemh2o(+=-o-z0sxkn4n9+zv!+n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
